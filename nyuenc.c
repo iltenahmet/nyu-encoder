29c2f56b36ddd89a165ff30ffbc8ba1ff8c541e7
Ahmet Ilten <iltenahmet@gmail.com>
2023-04-03 13:12:42 -0400

    Add simple run length encoding algorithm

--- nyuenc.c
+++ nyuenc.c
@@ -5,9 +5,9 @@ To use valgrind to check for memory leaks and track origins:
 
 valgrind --leak-check=full --track-origins=yes ./nyush
 
-To run a Docker container on Windows:
+To run a Docker container on WSL:
 
-docker run -i --name cs202 --privileged --rm -t -v C:\users\ailte\os:/cs202 -w /cs202 ytang/os bash
+docker run -i --name cs202 --privileged --rm -t -v /mnt/c/users/ailte/os:/cs202 -w /cs202 ytang/os bash
 
 To run a Docker container on Mac:
 
@@ -18,9 +18,46 @@ To zip specific files:
 zip nyush.zip Makefile *.h *.c
  */
 
+#include <stdio.h>
+#include <string.h>
 #include "nyuenc.h"
 
-int main()
-{
+#define MAX_FILE_COUNT 100
+//#define MAX_sda
 
+int main(int argc, char* argv[])
+{
+//    char* input = "aaaaaabbbbbbbbba";
+//
+//	FILE *file_ptr;
+//	char ch;
+//
+//	errno_t err_code = fopen_s(argv[1], "r");
+//	if (err_code != 0)
+//	{
+//		printf("Error reading the file");
+//		return 1;
+//	}
+
+    //RLE
+	char prev = input[0];
+	int count = 0;
+	size_t len = strlen(input);
+    for (size_t i = 0; i < len; i++)
+	{
+		char current = input[i];
+		if (current == prev)
+		{
+			count++;
+		} else
+		{
+			printf("%c%d", prev, count);
+			prev = current;
+			count = 1;
+		}
+	}
+	printf("%c%d", prev, count);
+
+	fclose(fiel_ptr);
+	return 0;
 }
