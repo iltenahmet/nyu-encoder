b80b2db63267facda15f38e610c59b387b30f79a
iltenahmet <iltenahmet@gmail.com>
2023-04-05 11:43:35 -0400

    Implement file reading, refactor RLE

--- nyuenc.c
+++ nyuenc.c
@@ -11,53 +11,84 @@ docker run -i --name cs202 --privileged --rm -t -v /mnt/c/users/ailte/os:/cs202 
 
 To run a Docker container on Mac:
 
-docker run -i --name cs202 --privileged --rm -t -v /users/ahmetilten/OS/labs:/cs202 -w /cs202 ytang/os bash
+docker run -i --name cs202 --privileged --rm -t -v /Users/ahmetilten/OS/labs:/cs202 -w /cs202 ytang/os bash
 
 To zip specific files:
 
-zip nyush.zip Makefile *.h *.c
+zip nyuenc.zip Makefile *.h *.c
  */
 
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "nyuenc.h"
 
-#define MAX_FILE_COUNT 100
-//#define MAX_sda
+#define INITIAL_INPUT_SIZE 100
 
-int main(int argc, char* argv[])
-{
-//    char* input = "aaaaaabbbbbbbbba";
-//
-//	FILE *file_ptr;
-//	char ch;
-//
-//	errno_t err_code = fopen_s(argv[1], "r");
-//	if (err_code != 0)
-//	{
-//		printf("Error reading the file");
-//		return 1;
-//	}
+int main(int argc, char *argv[]) {
 
-    //RLE
-	char prev = input[0];
-	int count = 0;
-	size_t len = strlen(input);
-    for (size_t i = 0; i < len; i++)
+	char* input = malloc(INITIAL_INPUT_SIZE * sizeof(char));
+	size_t size_needed = 0;
+
+	for (int i = 1; i <= argc; i++)
 	{
-		char current = input[i];
-		if (current == prev)
+		int fd = open(argv[1], O_RDONLY);
+		if (fd == -1)
+		{
+			fprintf(stderr, "Error opening the file.");
+			return 1;
+		}
+
+		struct stat sb;
+		if (fstat(fd, &sb) == -1)
+		{
+			fprintf(stderr, "Error getting file information.");
+			return 1;
+		}
+
+		size_needed += sb.st_size;
+		if (sizeof(input) < size_needed)
+		{
+			input = realloc(input, size_needed * sizeof(char));
+		}
+
+		char *addr = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+		if (addr == MAP_FAILED)
+		{
+			fprintf(stderr, "Error mapping the file into memory");
+			return 1;
+		}
+
+		close(fd);
+
+		//input += addr;
+		// add addr to input in a for loop
+	}
+
+	char* output = malloc(2 * size_needed * sizeof(char));
+
+	//Run Length Encoding
+	char current = input[0];
+	int count = 0;
+	int output_index = 0;
+	for (size_t i = 0; i < size_needed; i++)
+	{
+		if (input[i] == current)
 		{
 			count++;
-		} else
-		{
-			printf("%c%d", prev, count);
-			prev = current;
-			count = 1;
+			continue;
 		}
+		output_index += sprintf(&output[output_index], "%c%c", current, (char) count);
+		current = input[i];
+		count = 1;
 	}
-	printf("%c%d", prev, count);
+	output_index += sprintf(&output[output_index], "%c%c", current, (char) count);
+	write(STDOUT_FILENO, output, output_index);
 
-	fclose(fiel_ptr);
+	free(input);
+	free(output);
 	return 0;
 }
